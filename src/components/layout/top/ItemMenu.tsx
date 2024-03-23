25cf145a88735e7fc23cd987f379b5326a10b35b
Jose Carlos Sant Anna Junior <jcsj2010@gmail.com>
2024-03-23 14:59:48 -0300

    23.03.24

--- src/components/layout/top/ItemMenu.tsx
+++ src/components/layout/top/ItemMenu.tsx
@@ -9,7 +9,7 @@ interface ILinks {
 const ItemMenu = (props: ILinks) => {
   return (
     <Link
-      className={`${lato.className} border sm:text-lg md:text-xl antialiased text-zinc-950  hover:text-[#004e98] duration-150`}
+      className={`${lato.className} sm:text-lg md:text-xl antialiased text-zinc-950  hover:text-[#004e98] duration-150`}
       href={props.href}
     >
       {props.name}
