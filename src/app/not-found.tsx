cd2a342228ab64c0fb23c4d8da66af99492d4368
Jose Carlos Sant Anna Junior <jcsj2010@gmail.com>
2024-10-17 19:31:12 -0300

    17.10.24

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,14 @@
+import Image from "next/image";
 
 const PaginaNaoEncontrada = () => {
   return (
-    <div>PaginaNaoEncontrada</div>
-  )
-}
+    <div className="flex flex-col justify-center items-center h-screen  relative">
+      <div className="flex p-8">
+        DESCULPE EXPLORADOR, ESTA PAGINA NAO TEM SAIDA.{" "}
+      </div>
+      <Image src={"/img/1234.png"} alt="" width={400} height={400} />
+    </div>
+  );
+};
 
-export default PaginaNaoEncontrada
+export default PaginaNaoEncontrada;
