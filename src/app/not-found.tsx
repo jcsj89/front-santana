1479c839241e760d8413837b9eebe81d44c3e101
Jose Carlos Sant Anna Junior <jcsj2010@gmail.com>
2024-10-17 23:32:49 -0300

    page not-found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,29 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const PaginaNaoEncontrada = () => {
   return (
-    <div className="flex flex-col justify-center items-center h-screen  relative">
-      <div className="flex p-8">
-        DESCULPE EXPLORADOR, ESTA PAGINA NAO TEM SAIDA.{" "}
+    <div className="flex flex-col sm:justify-center items-center h-screen relative bg-slate-800 text-white">
+      <div className="flex flex-col justify-center items-center text-sm sm:text-2xl my-9">
+        <span>Este site, uma ilusão é.</span>
+        <span>Um mistério profundo, este link se tornou.</span>
+        <span>Voltar, você deve, escolha a luz e encontre a verdade,</span>
+        <span>antes que o lado sombrio da Força o encontre!</span>
       </div>
-      <Image src={"/img/1234.png"} alt="" width={400} height={400} />
+
+      <Image
+        className=" p-5 "
+        src={"/img/1234.png"}
+        alt=""
+        width={400}
+        height={400}
+      />
+      <Link
+        className="bg-green-500 text-white font-bold  text-md md:text-2xl rounded-full  text-center my-9 px-6 py-3"
+        href={"/"}
+      >
+        Voltar a Luz!
+      </Link>
     </div>
   );
 };
