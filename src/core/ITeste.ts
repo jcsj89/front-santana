9af1d212f72acda9586104aa6373164da938a1d8
Jose Carlos Sant Anna Junior <jcsj2010@gmail.com>
2024-11-03 20:28:59 -0300

    exclude tests folders

--- src/core/ITeste.ts
+++
@@ -1,3 +0,0 @@
-export interface ITeste {
-    nome: string
-}
--- src/core/teste2.ts
+++
@@ -1,8 +0,0 @@
-class Teste2 {
-    teste
-    constructor() {
-        this.teste = 'teste'
-    }
-}
-const t = new Teste2();
-t.teste
