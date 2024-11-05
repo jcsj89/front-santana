a7076b70bee443fabdea456306279acb0a6818c5
Ze Santanna <jcsj2010@gmail.com>
2024-11-05 16:07:12 -0300

    05.11.24

--- src/components/site/products/Phmetro.tsx
+++ src/components/site/products/Phmetro.tsx
@@ -53,7 +53,7 @@ const Phmetro = (props: PhmetroProps) => {
                 </p>
 
                 <div className="flex flex-col items-center lg:items-start my-4">
-                    <div className="flex gap-4 w-">
+                    <div className="flex gap-4 w-full border">
                         <div className="text-right border">ACIDO</div>
                         <div className="text-left border">0 - 6</div>
                     </div>
@@ -65,6 +65,24 @@ const Phmetro = (props: PhmetroProps) => {
                         <div className="text-right border">ACIDO</div>
                         <div className="text-left border">8 - 14</div>
                     </div>
+                </div>
+
+                <div className="grid grid-cols-2 grid-rows-3 gap-4 bg-blue-500">
+                    <div className="bg-sky-300">ACIDO</div>
+                    <div className="bg-sky-300">0 - 6</div>
+                    <div className="bg-sky-300">NEUTRO</div>
+                    <div className="bg-sky-300">7</div>
+                    <div className="bg-sky-300">ALCALINO</div>
+                    <div className="bg-sky-300">8 - 14</div>
+                </div>
+
+                <div className="grid grid-cols-5 grid-rows-5 gap-4">
+                    <div>1</div>
+                    <div>2</div>
+                    <div className="row-start-2">3</div>
+                    <div className="row-start-2">4</div>
+                    <div className="row-start-3">5</div>
+                    <div className="row-start-3">6</div>
                 </div>
             </div>
         </div>
