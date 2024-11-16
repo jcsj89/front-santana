5ae5056583931eb14f18e614542d0d637908dabb
Jose Carlos Sant Anna Junior <jcsj2010@gmail.com>
2024-11-16 12:20:34 -0300

    16.11.24

--- src/components/site/contact/Form.tsx
+++ src/components/site/contact/Form.tsx
@@ -191,7 +191,7 @@ const Form = () => {
                     className="text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-base px-10 py-6 text-center  dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800 duration-200
                     disabled:text-zinc-700 disabled:bg-white disabled:border-zinc-500 disabled:hover:text-zinc-700"
                     label="Enviar"
-                    // disabled={!canSubmit}
+                    disabled={!canSubmit}
                     type="submit"
                 />
             </div>
