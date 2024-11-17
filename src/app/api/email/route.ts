bb781371f57a60c5e705bd8041d86e465b367d1a
Jose Carlos Sant Anna Junior <jcsj2010@gmail.com>
2024-11-17 20:16:57 -0300

    17.11.24 20:17

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -1,15 +1,32 @@
 import { Email } from "@/components/shared/Email";
 import { resend } from "@/config/resend";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: Request): Promise<Response | undefined> {
-    const { data, error } = await resend.emails.send({
-        from: "Contato Site <noreply@santanadev.com.br>",
-        to: ["jcsj2010@gmail.com"],
-        subject: "Contato Formulario Site",
-        react: Email("santanaquimica"),
-        // html: "<strong>It works!</strong>",
-    });
+export async function GET(req: NextRequest): Promise<NextResponse | undefined> {
+    try {
+        console.log(req.body())
 
-    if (error) return Response.json(error);
-    Response.json(data);
+        const { data, error } = await resend.emails.send({
+            from: "Contato Site <noreply@santanadev.com.br>",
+            to: ["jcsj2010@gmail.com"],
+            subject: "Contato Formulario Site",
+            react: Email("https://www.santanaquimica.com.br"),
+        });
+
+        await Promise.resolve(data);
+
+        return NextResponse.json({
+            status: 200,
+            message: "Email sent correctly.",
+        });
+    } catch (error) {
+        if (error instanceof Error) {
+            return NextResponse.json({ error: error.message }, { status: 500 });
+        }
+
+        return NextResponse.json(
+            { error: "Something went wrong" },
+            { status: 500 }
+        );
+    }
 }
--- src/components/shared/Email.tsx
+++ src/components/shared/Email.tsx
@@ -1,13 +1,34 @@
-import { Body, Button, Html } from "@react-email/components";
+import {
+    Body,
+    Button,
+    Container,
+    Head,
+    Heading,
+    Html,
+    Preview,
+    Text,
+} from "@react-email/components";
 
 export function Email(props: any) {
     const { url } = props;
 
     return (
-        <Html lang="pt-BR">
-            <Body>
-                <Button href={url}>Clique meu site</Button>
-                {url}
+        <Html>
+            <Head />
+            <Preview>
+                Thank you for joining our waitlist and for your patience
+            </Preview>
+            <Body className="flex flex-col">
+                <Container className="">
+                    <Heading className="text-xl">Coming Soon.</Heading>
+                    <Button href={url}>Clique meu site</Button>
+                    {url}
+                    <Text className="text-sm text-blue-600">
+                        Thank you {url} for joining our waitlist and for your
+                        patience. We will send you a note when we have something
+                        new to share.
+                    </Text>
+                </Container>
             </Body>
         </Html>
     );
