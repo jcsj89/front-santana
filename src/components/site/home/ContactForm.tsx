a1caa64368a28d405840de6f92ca2fc494b716c9
Jose Carlos Sant Anna Junior <jcsj2010@gmail.com>
2024-11-20 23:05:23 -0300

    email

--- src/components/site/home/ContactForm.tsx
+++ src/components/site/home/ContactForm.tsx
@@ -32,6 +32,7 @@ const ContactForm = () => {
         refTurnstile.current?.reset(); // <------------- After each submit, recycling turnstile for next usage.
 
         const emailAPI = URL.concat("/api/email/send");
+        console.log(emailAPI);
         const request = new Request(emailAPI, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
@@ -110,7 +111,7 @@ const ContactForm = () => {
                     focus:ring-1 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-base h-12 px-10 py-1 text-center  dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800 duration-200
                     disabled:text-zinc-700 disabled:bg-white disabled:border-zinc-500 disabled:hover:text-zinc-700"
                         label="Enviar"
-                        disabled={!canSubmit}
+                        // disabled={!canSubmit}
                         type="submit"
                     />
                 </div>
