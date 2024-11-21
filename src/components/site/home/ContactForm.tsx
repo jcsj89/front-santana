2be71fd2e35030137a17d9f4b35420e12a1bcd7e
Jose Carlos Sant Anna Junior <jcsj2010@gmail.com>
2024-11-20 23:55:36 -0300

    email

--- src/components/site/home/ContactForm.tsx
+++ src/components/site/home/ContactForm.tsx
@@ -4,7 +4,6 @@ import { Turnstile, TurnstileInstance } from "@marsidev/react-turnstile";
 import { FormEvent, useEffect, useRef, useState } from "react";
 
 import RespEmailSent from "@/components/shared/RespEmailSent";
-import { URL } from "@/config/api";
 import Button from "../../UI/Button";
 import Input from "../../form/Input";
 
@@ -30,8 +29,6 @@ const ContactForm = () => {
     const handleSubmit = async (e: FormEvent) => {
         e.preventDefault();
         refTurnstile.current?.reset(); // <------------- After each submit, recycling turnstile for next usage.
-
-        const emailAPI = URL.concat("/api/email/send");
 
         const request = new Request("/api/email/send", {
             method: "POST",
@@ -111,7 +108,7 @@ const ContactForm = () => {
                     focus:ring-1 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-base h-12 px-10 py-1 text-center  dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800 duration-200
                     disabled:text-zinc-700 disabled:bg-white disabled:border-zinc-500 disabled:hover:text-zinc-700"
                         label="Enviar"
-                        // disabled={!canSubmit}
+                        disabled={!canSubmit}
                         type="submit"
                     />
                 </div>
