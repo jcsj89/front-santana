4485d672dfb7955f3fabab43730d36d5d22c2da6
Jose Carlos Sant Anna Junior <jcsj2010@gmail.com>
2024-11-20 23:16:35 -0300

    email

--- src/components/shared/RespEmailSent.tsx
+++ src/components/shared/RespEmailSent.tsx
@@ -1,6 +1,6 @@
 const RespEmailSent = () => {
     return (
-        <div className="flex flex-col justify-center gap-2 items-center w-full sm:w-2/3 md:w-1/2 border border-green-700 p-7 rounded-lg mt-2 ">
+        <div className="flex flex-col justify-center gap-2 items-center w-full sm:w-2/3 md:w-1/2 border border-green-700 p-7 rounded-lg mt-4 mb-7 ">
             <span className="text-xl text-green-500">
                 Email enviado com sucesso.
             </span>
